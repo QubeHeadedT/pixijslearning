d42129ec67b810b331a3e2174b6d0609d1991dd9
James Leech <j.leech@sonraianalytics.gmail.com>
2023-03-06 20:45:56 +0000

    Made clampy interactable. It's a game! :)

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -3,32 +3,49 @@ import { IScene, Manager } from "../Manager";
 
 export class GameScene extends Container implements IScene {
     private clampy: Sprite;
-    private clampyVelocity: number;
+    private clampySpeed: number;
+    private movement: number;
     constructor() {
         super();
 
-        // Inside assets.ts we have a line that says `"Clampy from assets.ts!": "./clampy.png",`
         this.clampy = Sprite.from("Clampy from assets.ts!");
+        this.movement = 0;
+
+        document.addEventListener("keydown", this.onKeyDown.bind(this));
+        document.addEventListener("keyup", this.onKeyUp.bind(this));
 
         this.clampy.anchor.set(0.5);
         this.clampy.x = Manager.width / 2;
         this.clampy.y = Manager.height / 2;
         this.addChild(this.clampy);
 
-        this.clampyVelocity = 5;
+        this.clampySpeed = 5;
     }
     public update(framesPassed: number): void {
         // Lets move clampy!
-        this.clampy.x += this.clampyVelocity * framesPassed;
+        this.clampy.x += this.movement * this.clampySpeed * framesPassed;
 
         if (this.clampy.x > Manager.width) {
             this.clampy.x = Manager.width;
-            this.clampyVelocity = -this.clampyVelocity;
         }
 
         if (this.clampy.x < 0) {
             this.clampy.x = 0;
-            this.clampyVelocity = -this.clampyVelocity;
+        }
+    }
+
+    private onKeyDown(e: KeyboardEvent): void {
+        console.log(e);
+        if (e.code === 'ArrowRight') {
+            this.movement = 1;
+        } else if (e.code === 'ArrowLeft') {
+            this.movement = -1;
+        } 
+    }
+
+    private onKeyUp(e: KeyboardEvent): void {
+        if (e.code === 'ArrowRight' || e.code === 'ArrowLeft') {
+            this.movement = 0;
         }
     }
 }
